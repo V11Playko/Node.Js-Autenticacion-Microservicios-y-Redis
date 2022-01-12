fc2b4e31bead43821d0a820798aa0841df3cc497
V11Playko <heinnervega20@gmail.com>
2022-01-12 13:04:29 -0500

    Documentación de nuestra API

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -1,44 +1,48 @@
-const express = require("express")
+const express = require('express');
 
-const response = require("../../../network/response");
-const Controller = require("./index")
+const response = require('../../../network/response');
+const Controller = require('./index');
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    Controller.list()
-        .then((list) => {
-            response.success(req, res, list, 200)
-        }).catch(err => {
-            response.error(req, res, err.message, 500)
-        })
-})
+// Routes
+router.get('/', list)
+router.get('/:id', get);
+router.post('/', upsert);
+router.put('/', upsert);
 
-router.get("/:id", (req, res) => {
+// Internal functions
+function list(req, res) {
+    Controller.list()
+        .then((lista) => {
+            response.success(req, res, lista, 200);
+        })
+        .catch((err) => {
+            response.error(req, res, err.message, 500);
+        });
+    
+}
+
+function get(req, res) {
     Controller.get(req.params.id)
         .then((user) => {
-            response.success(req, res, user, 200)
-        }).catch(err => {
-            response.error(req, res, err.message, 500)
+            response.success(req, res, user, 200);
         })
-})
+        .catch((err) => {
+            response.error(req, res, err.message, 500);
+        });
+    
+}
 
-router.get("/upsert/:id/:name", (req, res) => {
-    Controller.upsert(req.params.id, req.params.name)
+function upsert(req, res) {
+    Controller.upsert(req.body)
         .then((user) => {
-            response.success(req, res, user, 200)
-        }).catch(err => {
-            response.error(req, res, err.message, 500)
+            response.success(req, res, user, 201);
         })
-})
+        .catch((err) => {
+            response.error(req, res, err.message, 500);
+        });
+    
+}
 
-router.get("/delete/:id", (req, res) => {
-    Controller.remove(req.params.id)
-        .then((user) => {
-            response.success(req, res, user, 200)
-        }).catch(err => {
-            response.error(req, res, err.message, 500)
-        })
-})
-
-module.exports = router
+module.exports = router;
