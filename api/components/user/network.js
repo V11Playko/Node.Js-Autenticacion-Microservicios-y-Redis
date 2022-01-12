57ca7ad7cc118df962451361b0cdb7933bfdf564
V11Playko <heinnervega20@gmail.com>
2022-01-12 12:26:32 -0500

    Rutas para usuarios

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -1,13 +1,44 @@
-const express = require('express');
+const express = require("express")
 
-const response = require('../../../network/response');
-const Controller = require('./controller');
+const response = require("../../../network/response");
+const Controller = require("./index")
 
 const router = express.Router();
 
-router.get('/', function (req, res) {
-    const lista = Controller.list();
-    response.success(req, res, lista, 200);
+router.get("/", (req, res) => {
+    Controller.list()
+        .then((list) => {
+            response.success(req, res, list, 200)
+        }).catch(err => {
+            response.error(req, res, err.message, 500)
+        })
 })
 
-module.exports = router;
+router.get("/:id", (req, res) => {
+    Controller.get(req.params.id)
+        .then((user) => {
+            response.success(req, res, user, 200)
+        }).catch(err => {
+            response.error(req, res, err.message, 500)
+        })
+})
+
+router.get("/upsert/:id/:name", (req, res) => {
+    Controller.upsert(req.params.id, req.params.name)
+        .then((user) => {
+            response.success(req, res, user, 200)
+        }).catch(err => {
+            response.error(req, res, err.message, 500)
+        })
+})
+
+router.get("/delete/:id", (req, res) => {
+    Controller.remove(req.params.id)
+        .then((user) => {
+            response.success(req, res, user, 200)
+        }).catch(err => {
+            response.error(req, res, err.message, 500)
+        })
+})
+
+module.exports = router
