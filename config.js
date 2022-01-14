9ebdd62000b1b0f08f6873d795d40b106805996f
Heinner Vega <heinnervega20@gmail.com>
2022-01-14 14:25:01 -0500

    Base de datos real: MySQL

--- config.js
+++ config.js
@@ -5,4 +5,10 @@ module.exports = {
     jwt: {
         secret: process.env.JWT_SECRET || 'notasecret!',
     },
+    mysql: {
+        host: process.env.MYSQL_HOST || 'sql5.freemysqlhosting.net',
+        user: process.env.MYSQL_USER || 'sql5465668',
+        password: process.env.MYSQL_PASS || 'fzB2myUtni',
+        database: process.env.MYSQL_DB || 'sql5465668',
+    },
 }
