414a26bfcafb131fa38e12ca40b3cb0a2e6554ca
Heinner Vega <heinnervega20@gmail.com>
2022-01-15 14:54:43 -0500

    Separando la base de datos a un microservicio

--- config.js
+++ config.js
@@ -11,4 +11,7 @@ module.exports = {
         password: process.env.MYSQL_PASS || 'fzB2myUtni',
         database: process.env.MYSQL_DB || 'sql5465668',
     },
+    mysqlService: {
+        port: process.env.MYSQL_SRV_PORT || 3001,
+    }
 }
